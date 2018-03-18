116ec4d461b9166838cb4d752b7268c685414ab7
Tyler Thompson <tthompson@socialflare.us>
2018-03-18 16:47:24 -0500

    Adding client

---
+++ client.py
@@ -0,0 +1,34 @@
+# Python program to implement client side of chat room.
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(("127.0.0.1, 8080))
+
+while True:
+
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
+
+    '''There are two possible input situations. Either the
+    user wants to give  manual input to send to other people,
+    or the server is sending a message  to be printed on the
+    screen. Select returns from sockets_list, the stream that
+    is reader for input. So for example, if the server wants
+    to send a message, then the if condition will hold true
+    below.If the user wants to send a message, the else
+    condition will evaluate as true'''
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            server.send(message)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+server.close()
